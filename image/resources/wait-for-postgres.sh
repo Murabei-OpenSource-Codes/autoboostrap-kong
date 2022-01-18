cff43dfc08b51569e8e690a9988669c9564eb7f6
André Baceti <a.baceti@murabei.com>
2022-01-18 15:18:13 -0300

    :bug: wait for postgres script was not using enviroment variables to check connection.
    Change from default for  KONG_PG_PASSWORD, KONG_DATABASE lead infinite loop at wait-for-postgres.sh script

--- image/resources/wait-for-postgres.sh
+++ image/resources/wait-for-postgres.sh
@@ -4,7 +4,7 @@ echo "######################"
 echo "## Waiting Postgres ##"
 echo "######################"
 
-until PGPASSWORD=kong psql -h "$KONG_PG_HOST" -p 5432 -U kong -c '\q'; do
+until PGPASSWORD="${KONG_PG_PASSWORD}" psql -h "${KONG_PG_HOST}" -p 5432 -U "${KONG_DATABASE}" -c '\q'; do
   >&2 echo "Postgres is unavailable DB_HOST:${KONG_PG_HOST}, DB_PORT:5432, DB_USERNAME:kong: - sleeping"
   sleep 1
 done
