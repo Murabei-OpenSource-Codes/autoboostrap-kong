af8cc196ca359b231ae83db35444b33082896e76
Andrpe Baceti <a.baceti@murabei.com>
2025-04-23 17:49:15 -0300

    Building a new version for Kong Loadbalancer Gateway 3.9.0-ubuntu

--- image/resources/wait-for-postgres.sh
+++ image/resources/wait-for-postgres.sh
@@ -4,7 +4,7 @@ echo "######################"
 echo "## Waiting Postgres ##"
 echo "######################"
 
-until PGPASSWORD="${KONG_PG_PASSWORD}" psql -h "${KONG_PG_HOST}" -p 5432 -U "${KONG_PG_USER}" -c '\q'; do
+until PGPASSWORD="${KONG_PG_PASSWORD}" psql -h "${KONG_PG_HOST}" -p "${KONG_PG_PORT}" -U "${KONG_PG_USER}" -c '\q' ${KONG_PG_DATABASE}; do
   >&2 echo "Postgres is unavailable DB_HOST:${KONG_PG_HOST}, DB_PORT:5432, DB_USERNAME:kong: - sleeping"
   sleep 1
 done
