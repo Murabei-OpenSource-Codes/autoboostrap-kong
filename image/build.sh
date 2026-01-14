92d68d6f09df92cf77baf97dcf70b2886fd44009
André Andrade Baceti <a.baceti@murabei.com>
2026-01-14 16:12:00 -0300

    Building a new version for Kong Loadbalancer Gateway 3.9.0-ubuntu-0.0.1

--- image/build.sh
+++ image/build.sh
@@ -1,12 +1,3 @@
-# Update version path
-VERSION_FILE="version"
-current_version=$(grep -E "^VERSION=" "$VERSION_FILE" | cut -d'=' -f2)
-IFS='.' read -r major minor patch <<< "$current_version"
-patch=$((patch + 1))
-new_version="$major.$minor.$patch"
-sed -i "s/^VERSION=.*/VERSION=$new_version/" "$VERSION_FILE"
-echo "Updated version to $new_version"
-
 source version
 docker build -t andrebaceti/autoboostrap-kong:${VERSION} .
 
